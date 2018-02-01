f124b5517201ed76edeb502ad5a260555919f056
Paweł Koleśnik <pawelkolesnik@wp.pl>
2018-02-01 18:05:31 +0100

    add lifecycle methods
    add lifecycle methods

--- script.js
+++ script.js
@@ -6,16 +6,49 @@ var Counter = React.createClass ({
 	},
 
 	increment: function() {
-        this.setState({
-            counter: this.state.counter + 1
-        });
-    },
+		this.setState({
+			counter: this.state.counter + 1
+		});
+	},
 
 	decrement: function() {
-        this.setState({
-            counter: this.state.counter - 1
-        });
-    },
+		this.setState({
+			counter: this.state.counter - 1
+		});
+	},
+
+	getDefaultProps: function() {
+		console.log('Ustawienie domyślnych propsów, które nie zostały przekazane do komponentu');
+	},
+
+	componentWillMount: function() {
+		console.log('Jest wywoływany zaraz przed render, ustawienie stanu nie spowoduje przerenderowania komponentu');
+	},
+
+	componentDidMount: function() {
+		console.log('Po wykonaniu metody render wykonuje się ta metoda. Komponent jest już w drzewie DOM i można nim manipulować np poprzez jQuery');
+	},
+
+	componentWillReceiveProps: function() {
+		console.log('Wykonuje się gdy komponent dostaje nową właściwość, nie wykonuje się przy pierwszym renderowaniu');
+	},
+
+	shouldComponentUpdate: function() {
+		console.log('Sprawdza czy dany komponent trzeba jeszcze raz przerenderować (odświeżyć)');
+		return true;
+	},	
+
+	componentWillUpdate: function() {
+		console.log('Wykonuje się zaraz po tym jak metoda shouldComponentUpdate zwróci wartość true');
+	},
+
+	componentDidUpdate: function() {
+		console.log('tak jak w componentDidMount tylko po przerenderowaniu');
+	},
+
+	componentDidUpdate: function() {
+		console.log('Gdy usuwamy komponent to za pomocą tej metody możemy odpiąć nasłuchiwanie zdarzeń');
+	},
 
 	render: function() {
 		return React.createElement('div', {},
